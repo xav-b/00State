93384932eacddd3fe134d751aa09a3f6454f2eb0
Xavier Bruhière <xav@thib-K50AF.(none)>
2012-04-11 07:59:58 +0200

    [11-04-12]

--- src/opt_q/opt_q.c
+++ src/opt_q/opt_q.c
@@ -1,174 +1,66 @@
-/* 
- * Simulation de la commande PI
- * sur le modèle d'une MCC
- * Groupe M2AA007
- *	
- * TODOLIST: 
- * calculer le dépassement, la précision et les temps associés, analyse des résultats
- * intégrer les calculs par réduction quadratique des correcteurs
- * employer une méthode d'auto calcul plutôt que par saisie ? identification de la charge par comparaison avec l'attendu linéaire?
- * intégrer la mesure du courant (et sa limitation ?)
- * utiliser les graphiques 3D pour de meilleurs analyses 
- * intégrer d'autres types de charge
- * vérifier la validité du modèle
- * Faire de plus beaux graphiques (R ?)
- * Données du moteur dans fichier à part, ranger tout ça
- * graph interactif et animé
- * intégration du choix de la méthode d'asservissement
- * intégration des erreurs réelles (mesure, etc... cf code micropro)
- * optimisation
- * interface graphique
- * version matlab
- * 
- */
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <string.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#ifdef MTRACE
-#include <mcheck.h>
-#endif	/*  MTRACE  */
-
-#define	DELTA	0.01
-/*
- *#define	L1	20.60
- *#define	L2	76.64
- *#define	L3	3.16
- */
-
-#define MU	1
-#define TM	0.72
-#define	WN	157
-#define REF	6.28
-#define CN	18
-#define L	0.5
-#define M	0.5
-
-#define GNUPLOT_PATH "/usr/bin/gnuplot"
-
-typedef struct output output;
-struct output {
-	 float current;
-	 float position[2];
-	 float speed[2];
-};
-
-typedef struct correct correct;
-struct correct {
-	 int L1;
-	 int L2;
-	 int L3;
-};
-
-float cmd_i[2] = {0, 0};
-float return_int;
-double cp;
-
-void usage(char *prog_name, int exit_code) {
-	printf("Usage: %s <consign> <simu stop>\n", prog_name);
-	exit(exit_code);
-}
-
-void init(output *out_ptr) {
-	int k;
-
-	for (k=0; k < 2; k++) {
-		cmd_i[k] = 0;
-		out_ptr->position[k] = 0;
-		out_ptr->speed[k]=0;
-	}
-	out_ptr->current = 0;
-	cp = 0;
-	/*  Réduction des consignes et sorties ?  */
-}
-
-void get_pi(float consign, output *out_ptr, correct *gain_ptr) {
-	// BF
-	return_int = DELTA * (out_ptr->position[1] - consign) + return_int;
-	cmd_i[0] = (gain_ptr->L2 * consign - (gain_ptr->L2 * out_ptr->position[1] + gain_ptr->L1 * out_ptr->speed[1] + gain_ptr->L3 * return_int)) + M * L * 9.8 * cp / CN;
-}
-
-void mcc_model(output *out_ptr) {
-	out_ptr->speed[0] = (cmd_i[1] * (1 - exp(-MU * DELTA / TM)) / MU) + (out_ptr->speed[1] * exp(-MU * DELTA / TM));
-	out_ptr->position[0] = (DELTA * out_ptr->speed[1]) + out_ptr->position[1];
-}
-
-int plot_results() {
-	FILE *gp;
-
-	gp = popen(GNUPLOT_PATH, "w");
-	fprintf(gp, "load \"config\"\n");
-	fflush(gp);
-	getchar();
-	pclose(gp);
-
-	exit(EXIT_SUCCESS);
-}
+#include "opt_q.h"
 
 
 int main(int argc, char *argv[]) {
 	//mtrace();
 	//export MALLOC_TRACE=memory.log
-	float consign=0, max_pos=0, t=0, settled_time = 0;
-	int stop=0;
+	float max_pos=0, t=0, settled_time = 0;
+	float L2_save = 0, L3_save = 0;
+	float consign_r = 0;
 	int cpt = 0;
-	int i = 0;
+	int i = 0, k = 0;
 	char *datafile;
 	FILE* fd;
 	output val_out;
-	correct gain; 
+	conf vect;
 
 	printf("\n-----   Numeric simulation for MCC command   ------\n");
-	if (argc < 3)
-		usage(argv[0], 1);
 	
-	/*
-	 *init(&val_out, &gain);		//Global variables initialisation
-	 */
-	gain.L1 = 0;
-	gain.L2 = 0;
-	gain.L3 = 0;
-	consign=atoi(argv[1]) * 6.3 / 157; 
-	stop = atoi(argv[2]);
+	init(&val_out, &vect);		//Global variables initialisation
+	consign_r = vect.CONSIGN * WN / REF;
+	L2_save = vect.L2;
+	L3_save = vect.L3;
 	
 	datafile = (char *)malloc(20);
 	strcpy(datafile, "./results");
 	printf("[DEBUG] datafile @  %p: \'%s\'\n", datafile, datafile);
 	fd = fopen(datafile, "w");
 	
-	
-	while (gain.L1 < 30) {
-	    gain.L1 += 1;
-	    while (gain.L2 < 80) {
-	        gain.L2+=1;
-	        while (gain.L3 < 4) {
-	            gain.L3+=1;
+	while (vect.L1 < vect.L1_MAX) {
+	    vect.L1 += vect.L1_PAS;
+	    while (vect.L2 < vect.L2_MAX) {
+	        vect.L2+=vect.L2_PAS;
+	        while (vect.L3 < vect.L3_MAX) {
+	            vect.L3+=vect.L3_PAS;
+				printf("[DEBUG] New simulation [%2.2f - %2.2f - %2.2f]\n", vect.L1, vect.L2, vect.L3);
 				do {
-					cp = sin(val_out.position[0] * REF);
+					if (vect.CP_FLAG) {
+						cp = sin(val_out.position[0] * REF);
+						printf("[DEBUG] Computing load perturbation [%2.2f]\n", cp*vect.LENGTH*vect.WEIGTH);
+					}
+					else
+						cp = 0;
 
-					get_pi(consign, &val_out, &gain);	 //compute cmd_i[0] of current loop
-					mcc_model(&val_out);		 //compute output[0]
+					get_pi(vect.CONSIGN, &val_out, &vect, cp, t);	 //compute cmd_i[0] of current loop
+					mcc_model(&val_out, &vect, t);		 				//compute output[0]
 
-					t += DELTA;
+					t += vect.DELTA;
 				/*  increment indexes for next loop  */
 					val_out.speed[1] = val_out.speed[0];
 					val_out.position[1] = val_out.position[0];
 					cmd_i[1] = cmd_i[0];
 					
-					val_out.position[0] = val_out.position[0] * WN / REF;
-					val_out.speed[0] = val_out.speed[0];	//* WN
+					/*val_out.position[0] = val_out.position[0] * WN / REF;*/
+					/*val_out.speed[0] = val_out.speed[0];	// WN*/
 					/*
 					 *fprintf(fd, "%5.4f\t%5.4f\t%5.4f\t%2.2f\n", t, val_out.position[0], val_out.speed[0], cp);
 					 */
 					
-					if (fabs(val_out.position[0] - atoi(argv[1])) < (0.05*atoi(argv[1])))
+					if (fabs(val_out.position[0] - vect.CONSIGN) < (0.05*vect.CONSIGN))
 						   cpt++;
-					if (cpt == 50) {
-						settled_time = t-(cpt*DELTA);
-						/*printf("Settled in %2.2f\n", settled_time);*/
+					if (cpt == 60) {
+						settled_time = t-(cpt*vect.DELTA);
+						printf("[DEBUG] Settled in %2.2f\n", settled_time);
 						//break;
 						/*
 						 *cpt = 0;
@@ -177,30 +69,24 @@ int main(int argc, char *argv[]) {
 					if (val_out.position[0] > max_pos)
 						max_pos = val_out.position[0];
 
-				} while (t < stop); 
+				} while (t < vect.STOP); 
 				i++;
-				fprintf(fd, "%d\t%4.2f\t%2.2f\t%2.2f\t%d\t%d\t%d\n", i, fabs(max_pos - atoi(argv[1]))*100, settled_time, fabs(val_out.position[0] - atoi(argv[1]))*100, gain.L1, gain.L2, gain.L3);
-				/*fprintf(fd, "%d\t%4.2f\t%2.2f\t%2.2f\t%d\t%d\t%d\n", i, val_out.position[0], settled_time, fabs(val_out.position[0] - atoi(argv[1]))*100, gain.L1, gain.L2, gain.L3);*/
-				/*printf("L1: %d\tL2: %d\tL3: %d\n", gain.L1, gain.L2, gain.L3);*/
+				fprintf(fd, "%d\t%4.2f\t%2.2f\t%2.2f\t%2.2f\t%2.2f\t%2.2f\n", i, fabs(max_pos - vect.CONSIGN)*100*WN/REF, settled_time, fabs(val_out.position[0] - vect.CONSIGN)*100*WN/REF, vect.L1, vect.L2, vect.L3);
 				settled_time = 0;
 				max_pos = 0;
-				t = 0;
 				cpt = 0;
-				init(&val_out);		//Global variables initialisation
-	
+				for (k=0; k < 2; k++) {
+					cmd_i[k] = 0;
+					val_out.position[k] = 0;
+					val_out.speed[k]=0;
+				}
+				t = 0;
+				cp = 0;
 	         }
-	         gain.L3 = 0;
+			 vect.L3 = L3_save;
 	     }
-	     gain.L2 = 0;
+		 vect.L2 = L2_save;
 	 }
-	 
-	
-	 /*
-	  *printf("\n[DEBUG] loop for ended, closing file\n\n");
-	  *printf("position max atteinte: %2.2f\n", max_pos);
-	  *printf("Which is a %2.2f overshoot\n", fabs((max_pos - atoi(argv[1])))*100);
-	  *printf("erreur statique: %2.2f\n", fabs(val_out.position[0] - atoi(argv[1]))*100);
-	  */
 	 
 	fclose(fd);
 	free(datafile);
---
+++ src/opt_q/opt_q.h
@@ -0,0 +1,123 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#ifdef MTRACE
+#include <mcheck.h>
+#endif	/*  MTRACE  */
+#include "utils.h"
+
+#define MU	1
+#define TM	0.72
+#define	WN	157
+#define REF	6.28
+#define CN	18
+
+#define GNUPLOT_PATH "/usr/bin/gnuplot"
+
+typedef struct output output;
+struct output {
+	 float current;
+	 float position[2];
+	 float speed[2];
+};
+
+typedef struct conf conf;
+struct conf {
+	float L1;
+	float L2;
+	float L3;
+	float L1_PAS;
+	float L2_PAS;
+	float L3_PAS;
+	int L1_MAX;
+	int L2_MAX;
+	int L3_MAX;
+	float DELTA;
+	int CP_FLAG;
+	float LENGTH;
+	float WEIGTH;
+	float CONSIGN;
+	int STOP;
+	float SATURATION;
+	int SATURATION_FLAG;
+	FILE* fd_out;
+};
+
+float cmd_i[2] = {0, 0};
+float return_int;
+double cp;
+
+void usage(char *prog_name, int exit_code) {
+	printf("Usage: %s <consign> <simu stop>\n", prog_name);
+	exit(exit_code);
+}
+
+void init(output *out_ptr, conf *c_ptr) {
+	int k;
+	FILE* fd_conf;
+	char* conf_file;
+
+	printf("[DEBUG] Initialising Simulation...\n");
+	conf_file = (char *)ec_malloc(50);
+	strcpy(conf_file, "./input.conf");
+	if ((fd_conf = fopen(conf_file, "r")) == NULL)
+		fatal("opening input config file");
+	printf("[DEBUG] Loading vector input configuration [%p: \'%s\']\n", conf_file, conf_file);
+	fscanf(fd_conf, "%f %f %f %f %f %f %d %d %d %f %d %f %f %f %d %f %d", &(c_ptr->L1), &(c_ptr->L2), &(c_ptr->L3), &(c_ptr->L1_PAS), &(c_ptr->L2_PAS), &(c_ptr->L3_PAS), &(c_ptr->L1_MAX), &(c_ptr->L2_MAX), &(c_ptr->L3_MAX), &(c_ptr->DELTA), &(c_ptr->CP_FLAG), &(c_ptr->LENGTH), &(c_ptr->WEIGTH), &(c_ptr->CONSIGN), &(c_ptr->STOP), &(c_ptr->SATURATION), &(c_ptr->SATURATION_FLAG));
+
+	c_ptr->CONSIGN = c_ptr->CONSIGN * REF / WN;
+	for (k=0; k < 2; k++) {
+		cmd_i[k] = 0;
+		out_ptr->position[k] = 0;
+		out_ptr->speed[k]=0;
+	}
+	out_ptr->current = 0;
+	cp = 0;
+	fclose(fd_conf);
+	free(conf_file);
+	/*  Réduction des consignes et sorties ?  */
+}
+
+void get_pi(float consign, output *out_ptr, conf *c_ptr, float cp, float t) {
+	// BF
+	return_int = c_ptr->DELTA * (out_ptr->position[1] - consign) + return_int;
+	printf("[DEBUG] Computing integral return [%2.2f - %2.2f]\n", return_int, t);
+	cmd_i[0] = (c_ptr->L2 * consign - (c_ptr->L2 * out_ptr->position[1] + c_ptr->L1 * out_ptr->speed[1] + c_ptr->L3 * return_int)) + c_ptr->WEIGTH * c_ptr->LENGTH * 9.8 * cp / CN;
+	printf("[DEBUG] Computing command [%2.2f - %2.2f]\n", cmd_i[0], t);
+
+	if (c_ptr->SATURATION_FLAG) {
+		printf("[DEBUG] Check saturation [%2.2f - %2.2f]\n", c_ptr->SATURATION, t);
+		if (cmd_i[0] > c_ptr->SATURATION) {
+			printf("[DEBUG] Positive saturation [%2.2f - %2.2f]\n", c_ptr->SATURATION, t);
+			cmd_i[0] = c_ptr->SATURATION;
+		}
+		else if (cmd_i[0] < -(c_ptr->SATURATION)) {
+			printf("[DEBUG] Negative saturation [%2.2f - %2.2f]\n", -c_ptr->SATURATION, t);
+			cmd_i[0] = -(c_ptr->SATURATION);
+		}
+	}
+}
+
+void mcc_model(output *out_ptr, conf *c_ptr, float t) {
+	out_ptr->speed[0] = (cmd_i[1] * (1 - exp(-MU * c_ptr->DELTA / TM)) / MU) + (out_ptr->speed[1] * exp(-MU * c_ptr->DELTA / TM));
+	printf("[DEBUG] Computing WR [%2.2f - %2.2f]\n", out_ptr->speed[0], t);
+	out_ptr->position[0] = (c_ptr->DELTA * out_ptr->speed[1]) + out_ptr->position[1];
+	printf("[DEBUG] Computing ThetaR [%2.2f - %2.2f]\n", out_ptr->position[0], t);
+}
+
+int plot_results() {
+	FILE *gp;
+
+	gp = popen(GNUPLOT_PATH, "w");
+	printf("Figuring out gnuplot path...\n");
+	fprintf(gp, "load \"config\"\n");
+	printf("Loading gnuplot config file...\n");
+	fflush(gp);
+	getchar();
+	pclose(gp);
+
+	exit(EXIT_SUCCESS);
+}
